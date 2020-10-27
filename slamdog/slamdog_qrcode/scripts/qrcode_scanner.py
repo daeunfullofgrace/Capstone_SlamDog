fd99a867767110aa25ef4b881419fc633333b1c5
Daeun <38378873+pshsdu@users.noreply.github.com>
2020-10-27 17:20:51 +0900

    Add files via upload

--- slamdog/slamdog_qrcode/scripts/qrcode_scanner.py
+++ slamdog/slamdog_qrcode/scripts/qrcode_scanner.py
@@ -1,37 +1,26 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-
-<<<<<<< HEAD
 import os
 
-=======
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 from std_msgs.msg import Int32
 
 import rospy
 import cv2
 import qrcode_data_server
-<<<<<<< HEAD
 import stack
-=======
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 from nav_msgs.msg import Odometry
 import pyzbar.pyzbar as pyzbar
 from pyasn1.compat.octets import null
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-<<<<<<< HEAD
 from std_msgs.msg import String
-=======
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
 
 class QRcodeScanner:
     scanned_data = []
     destination_odem = [[null for col in range(6)] for row in range(10)]
 
-<<<<<<< HEAD
     index_stack = stack.Stack()
     odom_stack = stack.Stack()
 
@@ -40,14 +29,12 @@ class QRcodeScanner:
     flag = False
 
     file_loc = "/catkin_ws/src/slamdog/slamdog_qrcode/data/qr_code_data.txt"
-=======
     scanned_data_index = 0
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
     def __init__(self):
 
         rospy.loginfo("start!")
-<<<<<<< HEAD
+
         loc_list = (os.getcwd().split(os.path.sep))
         QRcodeScanner.file_loc = "/home/" + loc_list[2] + QRcodeScanner.file_loc
 
@@ -57,13 +44,11 @@ class QRcodeScanner:
 
         # Connect image topic
         # /raspicam_node/image
-=======
 
         self.bridge = CvBridge()
         self.image_received = False
 
         # Connect image topic
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
         img_topic = "/camera/rgb/image_raw"
         rospy.Subscriber(img_topic, Image, self.callback)
 
@@ -77,10 +62,7 @@ class QRcodeScanner:
     def callback(self, data):
         # print(is_btn_clicked)
         # Convert image to OpenCV format
-<<<<<<< HEAD
 
-=======
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
@@ -93,7 +75,6 @@ class QRcodeScanner:
         self.qrcode_decode(cv_image)
 
     def write_file(self, data):
-<<<<<<< HEAD
 
         if not QRcodeScanner.is_saved:
             f = open(QRcodeScanner.file_loc, 'w')
@@ -115,32 +96,8 @@ class QRcodeScanner:
 
     def qrcode_decode(self, img):
 
-        # if self.scanned_data_index == 0:
-        #     rospy.Subscriber('odom', Odometry, self.get_odom)
-=======
-        print(data)
-
-        output1 = QRcodeScanner.scanned_data
-        output2 = QRcodeScanner.destination_odem
-
-        f = open("/home/daeun/catkin_ws/src/slamdog/slamdog_qrcode/data/qr_code_data.txt", 'w')
-
-        for i in range(0, len(output1)):
-            data = "%" + str(output1[len(output1)-i-1]) + "\n"
-            f.write(data)
-
-            for j in range(0, 6):
-                data = str(output2[i][j]) + "\n"
-                f.write(data)
-
-        print("file saved!")
-        f.close()
-
-    def qrcode_decode(self, img):
-
         if self.scanned_data_index == 0:
             rospy.Subscriber('odom', Odometry, self.get_odom)
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
         if self.image_received:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -151,7 +108,6 @@ class QRcodeScanner:
                 barcode_data = d.data.decode("utf-8")
                 text = barcode_data
 
-<<<<<<< HEAD
                 # QRcodeScanner.scanned_data.append(text)
                 # QRcodeScanner.scanned_data = list(set(QRcodeScanner.scanned_data))
 
@@ -169,15 +125,6 @@ class QRcodeScanner:
                 #
                 #     self.print_in_list()
                 #     self.scanned_data_index += 1
-=======
-                QRcodeScanner.scanned_data.append(text)
-                QRcodeScanner.scanned_data = list(set(QRcodeScanner.scanned_data))
-
-                if self.scanned_data_index != len(QRcodeScanner.scanned_data):
-                    rospy.Subscriber('odom', Odometry, self.get_odom)
-                    self.print_in_list()
-                    self.scanned_data_index += 1
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
     def get_odom(self, msg):
 
@@ -190,7 +137,6 @@ class QRcodeScanner:
             quat_z = msg.pose.pose.orientation.z
             quat_w = msg.pose.pose.orientation.w
 
-<<<<<<< HEAD
             odom_list = [round(pos_x, 2), round(pos_y, 2), round(quat_x, 2), round(quat_y, 2), round(quat_z, 2), round(quat_w, 2)]
 
             # if QRcodeScanner.odom_stack.is_empty():
@@ -204,37 +150,10 @@ class QRcodeScanner:
                 QRcodeScanner.odom_stack.push(odom_list)
                 print(QRcodeScanner.odom_stack.peek())
                 QRcodeScanner.flag = False
-=======
-            QRcodeScanner.destination_odem[self.scanned_data_index][0] = round(pos_x, 2)
-            QRcodeScanner.destination_odem[self.scanned_data_index][1] = round(pos_y, 2)
-            QRcodeScanner.destination_odem[self.scanned_data_index][2] = round(quat_x, 2)
-            QRcodeScanner.destination_odem[self.scanned_data_index][3] = round(quat_y, 2)
-            QRcodeScanner.destination_odem[self.scanned_data_index][4] = round(quat_z, 2)
-            QRcodeScanner.destination_odem[self.scanned_data_index][5] = round(quat_w, 2)
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
         except():
             pass
 
-<<<<<<< HEAD
-=======
-    def print_in_list(self):
-
-        print("index : ", self.scanned_data_index)
-
-        for i in range(0, len(QRcodeScanner.scanned_data)):
-            if QRcodeScanner.scanned_data is not None:
-                print(QRcodeScanner.scanned_data[i], end="  ")
-
-        print()
-
-        for i in range(0, len(QRcodeScanner.destination_odem)):
-            if QRcodeScanner.destination_odem is not None:
-                print(QRcodeScanner.destination_odem[i], end=" ")
-
-        print()
-
->>>>>>> 608848bc16aceef6317965998409063c282f5d82
 
 if __name__ == '__main__':
 
