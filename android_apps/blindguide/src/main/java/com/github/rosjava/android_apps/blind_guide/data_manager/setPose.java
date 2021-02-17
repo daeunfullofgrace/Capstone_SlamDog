83137e136ae77732e3a8fdcf54031529632a3127
pshsdu <pshsdu75@gmail.com>
2021-02-17 18:46:57 +0900

    remove

--- android_apps/blindguide/src/main/java/com/github/rosjava/android_apps/blind_guide/data_manager/setPose.java
+++
@@ -1,7 +0,0 @@
-package com.github.rosjava.android_apps.blind_guide.data_manager;
-
-public interface setPose extends org.ros.internal.message.Message{
-    java.lang.String _TYPE = "com.github.rosjava.android_apps.blind_guide/data_manager/setPose";
-    java.lang.String _DEFINITION = "string a\n---\nstring result\n";
-
-}
