23b1624ee63a7ad61271bf20cad4839de30c1152
pshsdu <pshsdu75@gmail.com>
2021-02-17 16:05:28 +0900

    update

--- android_apps/blindguide/src/main/java/com/github/rosjava/android_apps/blind_guide/data_manager/dataLoader.java
+++
@@ -1,6 +0,0 @@
-package com.github.rosjava.android_apps.blind_guide.data_manager;
-
-public interface dataLoader extends org.ros.internal.message.Message{
-    java.lang.String _TYPE = "com.github.rosjava.android_apps.blind_guide/data_manager/dataLoader";
-    java.lang.String _DEFINITION = "string a\n---\nstring result\n";
-}
